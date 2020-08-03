c72337c767280b90e56229ff43beb9a61f7f9047
Kathirvel Chandrasekaran <kathirvel98@outlook.com>
2020-08-03 18:45:23 +0530

    auth firebase functions added

--- lib/services/auth.dart
+++ lib/services/auth.dart
@@ -1,3 +1,48 @@
+import 'package:firebase_auth/firebase_auth.dart';
+import 'package:flutter_firebase/modal/user.dart';
+
 class AuthMethods {
-  
+  final FirebaseAuth _auth = FirebaseAuth.instance;
+
+  User _userFromFire(FirebaseUser user) {
+    return user != null ? User(userId: user.uid) : null;
+  }
+
+  Future signInWithEmail(String email, String password) async {
+    try {
+      AuthResult res = await _auth.signInWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = res.user;
+      return _userFromFire(user);
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  Future signUpWithEmail(String email, String password) async {
+    try {
+      AuthResult res = await _auth.createUserWithEmailAndPassword(
+          email: email, password: password);
+      FirebaseUser user = res.user;
+      return _userFromFire(user);
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  Future resetPassword(String email) async {
+    try {
+      return await _auth.sendPasswordResetEmail(email: email);
+    } catch (err) {
+      print(err);
+    }
+  }
+
+  Future signOut() async {
+    try {
+      return await _auth.signOut();
+    } catch (err) {
+      print(err);
+    }
+  }
 }
